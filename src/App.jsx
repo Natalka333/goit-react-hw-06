70f5006fac802766136944ee4941b53813a058e4
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2024-08-23 17:57:40 +0300

    add persistor

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,17 @@
-// import { useEffect, useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { addContact, deleteContact } from './redux/contactsSlice'
-
-import './App.css'
-
 import ContactList from './components/ContactList/ContactList';
 import SearchBox from './components/SearchBox/SearchBox';
 import ContactForm from './components/ContactForm/ContactForm';
+import Title from './components/Title/Title'
 
-// import initialContacts from './components/data/data.json'
+import './App.css'
+
 
 
 const App = () => {
-  // const dispatch = useDispatch();
-
-  // const [contacts, setContacts] = useState(() => {
-  //   const savedContact = localStorage.getItem('contactsKey')
-  //   return savedContact ? JSON.parse(savedContact) : initialContacts;
-  // })
-
-  // const [inputValue, setInputValue] = useState("")
-
-
-  // useEffect(() => {
-  //   window.localStorage.setItem('contactsKey', JSON.stringify(contacts))
-  // }, [contacts])
-
-  // const handleDelete = (id) => {
-  //   dispatch(deleteContact(id));
-  // }
-
-
-
 
   return (
     <div>
-      <h1>Phonebook</h1>
+      <Title />
       <ContactForm />
       <SearchBox />
       <ContactList />
