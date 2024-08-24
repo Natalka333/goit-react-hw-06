4785e28df703d3e5aa03afdcd6dec059a105a22c
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2024-08-24 16:47:04 +0300

    fix search box

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,12 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { changeFilter } from '../../redux/filtersSlice';
 
 import css from './SearchBox.module.css'
 
 const SearchBox = () => {
     const dispatch = useDispatch();
+    const filterValue = useSelector(state => state.filter.name);
+
 
     const handleChange = (event) => {
         dispatch(changeFilter(event.target.value))
@@ -16,6 +18,7 @@ const SearchBox = () => {
             <p className={css.search}>Find contacts by name</p>
             <input
                 type="text"
+                value={filterValue}
                 onChange={handleChange}
             />
 
